d8b26e53130d81cc00b8138d370fe076d332b33c
Luis Reyes <59459778+LuisReyes97@users.noreply.github.com>
2023-02-14 00:22:19 -0600

    lr

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'project-spa',
-    title: 'aryy-spa',
+    titleTemplate: 'Blanquita & Hannyta',
+    title: 'Blanquita & Hannyta',
     htmlAttrs: {
       lang: 'en'
     },
