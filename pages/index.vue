0384eef3b2f7686ad8b8448d4e58dd2dcc1b0852
Luis Reyes <59459778+LuisReyes97@users.noreply.github.com>
2023-02-13 23:11:49 -0600

    Luis Reyes

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-        <v-card>Hannyta</v-card>
+        <v-card>Luis Reyes</v-card>
       <v-btn @click="$vuetify.theme.dark = ! $vuetify.theme.dark ">dark</v-btn>
     </v-col>
   </v-row>
